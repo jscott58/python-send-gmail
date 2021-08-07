1835f0e9c1b5a3f05720e0fc747e5fff119f0479
jscott59 <scottcalhoun65@gmail.com>
2021-08-07 13:45:02 -0500

    fixed credentials.json and token.json access - deployed using windows task scheduler

--- sendmessage.py
+++ sendmessage.py
@@ -5,12 +5,13 @@ import sys
 import time
 import datetime
 import base64
-import pathlib
+import glob
 from email.mime.text import MIMEText
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from oauth2client.service_account import ServiceAccountCredentials
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 
@@ -21,6 +22,9 @@ gmail_sender = "scottcalhoun65@gmail.com"
 gmail_to = "scott.calhoun@hbkeso.com"
 gmail_subject = "Utilization Run Check"
 gmail_userid = "scottcalhoun65@gmail.com"
+sftp_host = "sftp.hbkeso.com"
+sftp_username = "HBKUtil"
+sftp_password = "^8y5Sc1F3%e89xZ2#blkW6pn0(Ym)R@s1!G"
 
 
 def resource_path(relative_path):
@@ -74,6 +78,8 @@ def send_message(service, user_id, message):
 
 def main():
     gmail_text = ""
+    local_dir = ".\\accountroot"
+    filelist = glob.glob(".\\accountroot\\*csv")
     print(astline)
     print("HBK Solutions LLC Unanet Utilization Report - Run Check")
     print(astline)
@@ -83,78 +89,83 @@ def main():
     print("Reading HBKUtil utilization folder on sftp.hbkeso.com")
     print(astline)
     print("")
+    # Setup local accountroot folder if not eexisit
+    if os.path.isdir(local_dir):
+        for filepath in filelist:
+            os.remove(filepath)
+    else:
+        os.mkdir(local_dir)
     # Define hostkey path so that pyinstaller will correctly include when using --onrfile
     sftphostkey_path = resource_path("hbkeso.pub")
-    # ASpecify host jey for sftp server
+    # A Specify host jey for sftp server
     cnopts = pysftp.CnOpts(knownhosts=sftphostkey_path)
     # And authenticate with a private key
     with pysftp.Connection(
-        host="sftp.hbkeso.com",
-        username="HBKUtil",
-        password="^8y5Sc1F3%e89xZ2#blkW6pn0(Ym)R@s1!G",
+        host=sftp_host,
+        username=sftp_username,
+        password=sftp_password,
         cnopts=cnopts,
     ) as sftp:
         try:
             utilFiles = sftp.listdir("./")  # upload file to public/ on remote
             print(astline)
-            print("")
             if len(utilFiles) == 0:
                 print("No files found.")
             else:
                 print("sftp files:")
+                sftp.get_d('.', local_dir, preserve_mtime=True)
                 # printing the list using loop
-                for x in range(len(utilFiles)):
-                    print("      " + utilFiles[x])
-                    fileStatsObj = os.stat(utilFiles[x])
-                #    modificationTime = time.ctime(fileStatsObj.st_mtime)
-                    sftp.get(utilFiles[x],"C:\\Users\\jscott\\PythonProjects\\python-send-gmail\\utilFile.csv", preserve_mtime=True)
-                #   sftp.get(utilFiles[x], "C:\\Users\\jscott\\PythonProjects\\python-send-gmail\\utilFile.csv")
-                    utilFileStatsObj = os.stat('utilFile.csv')
+                for f in os.listdir(local_dir):
+                    print(f)
+                    utilFileStatsObj = os.stat(os.path.join(local_dir, f))
                     modificationTime = time.ctime(utilFileStatsObj.st_mtime)
-                #    mtime = utilFile.stat().st_ctime
-                #    timestamp_str = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d-%H:%M')
-                #    print(timestamp_str)
-                    print("      Last Modified: ", modificationTime)
-                    gmail_text += utilFiles[x] + "\r" + "      Last Modified: " + modificationTime
-            print("")
+                    print("    Last Modified: ", modificationTime)
+                    gmail_text += f + "\n" + "      Last Modified: " + modificationTime + "\n\r" 
             print(astline)
-            creds = None
-            # The file token.json stores the user's access and refresh tokens, and is
-            # created automatically when the authorization flow completes for the first
-            # time.
-            if os.path.exists('token.json'):
-                creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-            # If there are no (valid) credentials available, let the user log in.
-            if not creds or not creds.valid:
-                if creds and creds.expired and creds.refresh_token:
-                    creds.refresh(Request())
+            print("")
+            try:
+                creds = None
+                # The file token.json stores the user's access and refresh tokens, and is
+                # created automatically when the authorization flow completes for the first
+                # time.
+                if os.path.exists(os.path.join(local_dir,'token.json')):
+                    creds = Credentials.from_authorized_user_file(os.path.join(local_dir,'token.json'), SCOPES)
+                # If there are no (valid) credentials available, let the user log in.
+                if not creds or not creds.valid:
+                    if creds and creds.expired and creds.refresh_token:
+                        creds.refresh(Request())
+                    else:
+                        flow = InstalledAppFlow.from_client_secrets_file(
+                            resource_path('credentials.json'), SCOPES)
+                        creds = flow.run_local_server(port=0)
+                        # creds = ServiceAccountCredentials.from_json_keyfile_name(resource_path('credentials.json'), SCOPES)
+                    # Save the credentials for the next run
+                    with open(os.path.join(local_dir,'token.json'), 'w') as token:
+                        token.write(creds.to_json())
+
+                service = build('gmail', 'v1', credentials=creds)
+
+                # Call the Gmail API
+                results = service.users().labels().list(userId='me').execute()
+                labels = results.get('labels', [])
+                print(astline)
+                if not labels:
+                    print('No labels found.')
                 else:
-                    flow = InstalledAppFlow.from_client_secrets_file(
-                        'credentials.json', SCOPES)
-                    creds = flow.run_local_server(port=0)
-                # Save the credentials for the next run
-                with open('token.json', 'w') as token:
-                    token.write(creds.to_json())
-
-            service = build('gmail', 'v1', credentials=creds)
-
-            # Call the Gmail API
-            results = service.users().labels().list(userId='me').execute()
-            labels = results.get('labels', [])
-
-            if not labels:
-                print('No labels found.')
-            else:
-                print('Gmail Account Found')
-                # enable label print to debug access to gmail account
-                # print('Labels:')
-                # for label in labels:
-                #     print(label['name'])
-                    
-                gmail_message = create_message(gmail_sender, gmail_to, gmail_subject, gmail_text)
-                send_message(service, gmail_userid, gmail_message)
+                    print('Gmail Account Found')
+                    # enable label print to debug access to gmail account
+                    # print('Labels:')
+                    # for label in labels:
+                    #     print(label['name'])
+                        
+                    gmail_message = create_message(gmail_sender, gmail_to, gmail_subject, gmail_text)
+                    send_message(service, gmail_userid, gmail_message)
+                    # print(gmail_text)
+                print(astline)
+            except ValueError:
+                    print("Googe API ERROR: " + ValueError)
         except ValueError:
-            print("SFTP listdir ERROR - EXITING")
+            print("SFTP ERROR - EXITING: " + ValueError)
             print(astline)
 
 
